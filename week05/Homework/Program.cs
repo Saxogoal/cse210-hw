6a2c4245124b95a1200c8fb822eb170d4d7087f7
Saxogoal <andrepiros01@gmail.com>
2026-04-02 16:03:43 -0300

    finished the learning activity

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        //Creating a base assignment
+        Assignment a1 = new Assignment("John Doe", "Math");
+        Console.WriteLine(a1.GetSummary()); 
+
+        //Creating the derived class assignments
+        MathAssignment a2 = new MathAssignment("John Doe", "Math", "Section 5.2", "Problems 1-10");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("John Doe", "English", "The Great Gatsby");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
